6194735dcc409e2a733ec6c25138782f4761992c
TheAwiteb <awiteb@hotmail.com>
2022-12-23 22:13:27 +0300

    Add copyright

--- src/app/commands/keypair/default.rs
+++ src/app/commands/keypair/default.rs
@@ -1,3 +1,19 @@
+// Solwalrs, A simple and easy to use CLI Solana wallet
+// Copyright (C) 2022  Solwalrs contributors <https://github.com/TheAwiteb/solwalrs/graphs/contributors>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+
 use clap::Parser;
 
 use crate::app::AppArgs;
